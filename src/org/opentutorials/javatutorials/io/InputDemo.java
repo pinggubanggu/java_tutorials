8ee2fa82df562fe9ac8171286e54237ab3d80b34
duddn <duddn@LAPTOP-O4DS5DH1>
2021-02-06 18:44:06 +0900

    io 변경

--- src/org/opentutorials/javatutorials/io/InputDemo.java
+++ src/org/opentutorials/javatutorials/io/InputDemo.java
@@ -3,7 +3,7 @@ package org.opentutorials.javatutorials.io;
 public class InputDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(io);
 
 	}
 
